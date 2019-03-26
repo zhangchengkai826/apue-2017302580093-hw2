935ed1aad994450626af4657530d747cc35b2c8f
Chengkai Zhang <zchengkai826@gmail.com>
2019-03-26 09:01:10 +0800

    code skeleton

--- ls.c
+++ ls.c
@@ -1,8 +1,27 @@
 #include "common.h"
+#include "getopt.h"
+
+typedef unsigned char bool;
+static bool opts[128];
+
+void doTask(const char *fn);
 
 int main(int argc, char *argv[]) {
-  argc += 2;
-  argv[0][0] = 0;
+  int opt, i;
+  bool hsopt = 0;
+  while((opt = getopt(argc, argv, "AacCdFfhiklnqRrSstuwx1")) != -1) {
+    opts[opt] = 1;
+    hsopt = 1;
+  }
+  if(hsopt) i = 2;
+  else i = 1;
+  for(; i < argc; i++) {
+    doTask(argv[i]);
+  }
   exit(0);
 }
 
+void doTask(const char *fn) {
+  fputs(fn, stdout);
+  fputs("\n", stdout);
+}
