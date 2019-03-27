83e907bbf33660c0778940bb41ef1e4878df0452
Chengkai Zhang <zchengkai826@gmail.com>
2019-03-27 18:07:25 +0800

    ls.c: add mutual exclusion & terminal judge support on command line parsing

--- ls.c
+++ ls.c
@@ -3,25 +3,40 @@
 
 typedef unsigned char bool;
 static bool opts[128];
+static bool is_trmnl; /* is_stdout_terminal? */
 
 void doTask(const char *fn);
 
 int main(int argc, char *argv[]) {
   int opt, i;
   bool hsopt = 0;
+  if(isatty(STDOUT_FILENO))
+    is_trmnl = 1;
+  else
+    is_trmnl = 0;
+  if(is_trmnl) {
+    opts['C'] = 1;
+    opts['q'] = 1;
+  } else {
+    opts['1'] = 1;
+    opts['w'] = 1;
+  }
+
   while((opt = getopt(argc, argv, "AacCdFfhiklnqRrSstuwx1")) != -1) {
+    if(opt == 'w' || opt == 'q')
+      opts['w'] = opts['q'] = 0;
+    else if(opt == 'l' || opt == 'C' || opt == '1' || opt == 'n' || opt == 'x')
+      opts['l'] = opts['C'] = opts['1'] = opts['n'] = opts['x'] = 0;
+    else if(opt == 'c' || opt == 'u')
+      opts['c'] = opts['u'] = 0;
     opts[opt] = 1;
     hsopt = 1;
   }
+  printf("l=%d, C=%d, 1=%d, n=%d, x=%d\n", opts['l'], opts['C'], \
+      opts['1'], opts['n'], opts['x']);
   if(hsopt) i = 2;
   else i = 1;
-  for(; i < argc; i++) {
-    doTask(argv[i]);
-  }
+  i++;
   exit(0);
 }
 
-void doTask(const char *fn) {
-  fputs(fn, stdout);
-  fputs("\n", stdout);
-}
